5db4ff8f105c6b0eecabb93d67b7372e757a4fe2
altanai <tara181989@gmail.com>
2018-10-30 11:02:11 +0530

    Initial Commit

---
+++ plivoIosQuickStart/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  plivoIosQuickStart
+//
+//  Created by Altanai Bisht on 30/10/18.
+//  Copyright © 2018 Altanai Bisht. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
